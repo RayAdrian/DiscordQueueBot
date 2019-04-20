46a8014bd6f4c371c2b0db6e1e64f3251d6f3e21
RayAdrian <35563637+RayAdrian@users.noreply.github.com>
2019-04-20 12:13:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var full = 0;
 
 bot.on('ready', () => {
     console.log('Bot is online');
-    bot.user.setActivity("CSbot; type .help for info");
+    bot.user.setActivity("CSbot; type .help for info.");
 });
 
 bot.on('message' ,msg=>{
