d750dfa863be2b0259898d11c3794871eb5c087b
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-09-05 23:36:08 +0800

    Refactor main app index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { Client, Message, TextChannel } from 'discord.js';
+import { Client } from 'discord.js';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import { LocalCache } from './caches';
 import { processCommand } from './commands';
-import { INFO_MSG_TIME_DEL, PREFIX } from './common/constants';
-import { deleteMessage, sendMessage } from './utils';
+import { PREFIX } from './common/constants';
+import sendErrorMessage from './utils/sendErrorMessage';
+import sendInfoMessage from './utils/sendInfoMessage';
 
 // For local development
 dotenv.config();
@@ -23,35 +24,30 @@ const localCache = new LocalCache();
  * Setup and run bot
  */
 bot.on('ready', () => {
-    console.log('Bot is online');
     if (bot && bot.user) {
         bot.user.setActivity(".help | Sup gamers");
-        sendMessage(
-            bot.channels.cache.get(process.env.INFO_CHANNEL_ID) as TextChannel,
-            'Bot is online',
-            (sentMessage : Message) => deleteMessage(sentMessage, INFO_MSG_TIME_DEL),
-        );
+        sendInfoMessage(bot, 'Bot is online');
     }
 
-    mongoose.connect(process.env.DB_URL).then(() => {
-        console.log('Connected to MongoDB');
-        sendMessage(
-            bot.channels.cache.get(process.env.INFO_CHANNEL_ID) as TextChannel,
-            'Connected to MongoDB',
-            (sentMsg : Message) => deleteMessage(sentMsg, INFO_MSG_TIME_DEL),
-        );
-        localCache.fetch();
+    mongoose.connect(process.env.DB_URL)
+        .then(() => {
+            sendInfoMessage(bot, 'Connected to MongoDB');
+            localCache.fetch();
+        }) 
+        .catch((error : Error) => sendErrorMessage(bot, error));
+});
+
+if (process.env.ALLOW_LEGACY_MESSAGING) {
+    bot.on('message', (message) => {
+        if (message.author == bot.user) { // Prevent bot from responding to its own messages
+            return;
+        }
+
+        // original manual method with PREFIX ('.' by default)
+        if (message.content.startsWith(PREFIX)) {
+            processCommand(bot, localCache, message);
+        }
     });
-});
-
-bot.on('message', (message) => {
-    if (message.author == bot.user) { // Prevent bot from responding to its own messages
-        return;
-    }
-
-    if (message.content.startsWith(PREFIX)) {
-        processCommand(localCache, message);
-    }
-});
+}
 
 bot.login(process.env.BOT_TOKEN);
