5d0337eaffb65f138e5010ba8cbbd097c908a245
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-09-05 23:35:22 +0800

    Update constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,10 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const PREFIX = '.'; // TODO: Replace with slash commands
 
 export const MSG_TIME_DEL = 3000;
 export const MSG_TIME_FULL_DEL = 7000;
 export const INFO_MSG_TIME_DEL = 60000;
-export const MAIN_CHANNEL_ID = process.env.CHANNEL_ID; // Pro-Gaming Channel
-export const INFO_CHANNEL_ID = process.env.INFO_CHANNEL_ID;
+export const MAIN_CHANNEL_ID = process.env.CHANNEL_ID || '';
+export const INFO_CHANNEL_ID = process.env.INFO_CHANNEL_ID || '';
+export const ERROR_CHANNEL_ID = process.env.ERROR_CHANNEL_ID || '';
 
 export const PUNISH_TAG = '<@&747740479638208532>';
 export const PUNISH_ID = '747740479638208532'
