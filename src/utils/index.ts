b46b94c646eae8b2aef0f7c7aec3542d098e0c5b
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-09-06 16:42:05 +0800

    Barrel util functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,6 @@
 export { default as deleteMessage } from './deleteMessage';
+export { default as isValidRole } from './isValidRole';
+export { default as sendErrorMessage } from './sendErrorMessage';
+export { default as sendInfoMessage } from './sendInfoMessage';
 export { default as sendMessage } from './sendMessage';
+export { default as sendMessageEmbed } from './sendMessageEmbed';
