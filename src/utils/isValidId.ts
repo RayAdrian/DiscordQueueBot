7e2146382b5c2cd326fc9591694ce549187177d0
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-10-05 00:58:27 +0800

    Revert "Change isValidRole util to isValidId"
    This reverts commit c4c11758993fd84731f546c6c2cf8082b09c40b9.

--- src/utils/isValidId.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Checks whether a string is a valid (discord) id
- * @param id 
- */
-export default function isValidId(id : string) : boolean {
-    const idRegex = /^<@&[0-9]+>$/i; // id format: <@&1234567890>
-    return idRegex.test(id);
-}
