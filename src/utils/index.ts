fcf5d7c83b33d0745ed22d64104790192420ae69
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-10-05 01:17:29 +0800

    Add util for validating user ids

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export { default as deleteMessage } from './deleteMessage';
 export { default as isValidLimit } from './isValidLimit';
 export { default as isValidRole } from './isValidRole';
+export { default as isValidUser } from './isValidUser';
 export { default as sendErrorMessage } from './sendErrorMessage';
 export { default as sendInfoMessage } from './sendInfoMessage';
 export { default as sendMessage } from './sendMessage';
--- src/utils/isValidRole.ts
+++ src/utils/isValidRole.ts
@@ -1,6 +1,7 @@
 /**
  * Checks whether the role is valid
- * @param role 
+ * Sample id: <@&1234567890>
+ * @param role
  */
 export default function isValidRole(role : string) : boolean {
     const roleRegex = /^<@&[0-9]+>$/i; // role id format: <@&1234567890>
