b2af0a2a3320a55ba00b872f49656ffb43f2e7b1
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-10-26 00:58:49 +0800

    Update user model/schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,60 @@
 import mongoose from 'mongoose';
 
-interface IUser {
-    id: string;
-    games: string[];
+export interface IUser {
+    name: string;
+    gameNames: string[];
 };
 
+export class User {
+    private name: string;
+    private gameNames: Set<string>;
+
+    constructor(name : string, gameNames : Array<string> = []) {
+        this.name = name;
+        this.gameNames = new Set(gameNames);
+    }
+
+    getUserWrapper() : IUser {
+        return {
+            name: this.name,
+            gameNames: [...this.gameNames],
+        }
+    }
+
+    getName() : string {
+        return this.name;
+    }
+
+    getGameNames() : Array<string> {
+        return [...this.gameNames];
+    }
+
+    hasGame(gameName : string) : boolean {
+        return this.gameNames.has(gameName);
+    }
+
+    addGameNames(gameNames : Array<string>) : void {
+        gameNames.forEach((gameName) => this.gameNames.add(gameName));
+    }
+
+    deleteGameNames(gameNames : Array<string>) : void {
+        gameNames.forEach((gameName) => this.gameNames.delete(gameName));
+    }
+
+    clear() : void {
+        this.gameNames.clear();
+    }
+}
+
 const userSchema = new mongoose.Schema<IUser>({
-    id: String,
-    games: [String]
+    name: {
+        type: String,
+        required: true,
+    },
+    gameNames: {
+        type: Array<String>,
+        default: [],
+    },
 });
 
 export default mongoose.model<IUser>('User', userSchema);
