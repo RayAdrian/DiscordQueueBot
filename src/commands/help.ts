0cef89c29f0acb2d03c01d356c0af04d0325ff6b
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-11-05 11:43:24 +0800

    Update help command to improve visibility and readability
    Send simplified msg as a reply, and a comprehensive msg through DM

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,21 +1,22 @@
 import { Message, MessageEmbed } from 'discord.js';
-import { PREFIX } from '../common/constants';
-import { sendMessage } from '../utils';
+import { INFO_MSG_TIME_DEL, PREFIX } from '../common/constants';
+import { deleteMessage, sendMessage } from '../utils';
 import gameCommands from './game';
 import lineupCommands, { specialJoinCommand } from './lineup';
 import { CommandInputs } from './processCommand';
 import userCommands from './user';
 
-const getDescriptions = (commands) : Array<{ name : string, value : string }> => {
+const getDescriptions = (commands) : Array<{ name : string, description : string, simpleDescription }> => {
     return commands
         .filter(({ name, formats, descriptions }) => (name?.length && formats?.length && descriptions?.length))
         .map(({ name, aliases = ['None'], formats, descriptions }) => ({
             name,
-            value: `
+            description: `
                 *Aliases: ${aliases.join(', ')}*\n
                 ${formats.map((format, index) => `\`${PREFIX}${format}\`\n${descriptions[index]}\n`).join('\n')}
                 \u200b
             `,
+            simpleDescription: formats.map((format, index) => `${name} ${index + 1} - \`${format}\``).join('\n'),
         }));
 };
 
@@ -27,15 +28,21 @@ const helpDescriptions = {
 
 /**
  * Function for the help command
- * Sends list of possible commands each with description/s
- * @param parameters - contains the necessary parameters for the command
+ * Sends list of possible commands, a simplified one as a reply, and a comprehensive one through DM
+ * @param commandInputs - contains the necessary parameters for the command
  */
 function help(commandInputs : CommandInputs) {
     const { message } : { message : Message } = commandInputs;
 
+    const simpleHelpEmbed = new MessageEmbed()
+        .setTitle('GentleBot Help')
+        .addField('Comprehensive Help', 'For a comprehensive help message, check your DM\'s.');
     const helpEmbed = new MessageEmbed()
         .setTitle('GentleBot Help')
-        .addField('Queueing Commands', 'e.g. \`.help\`')
+        .addField('Queueing Commands', `
+            Command formats listed under the aliases, highlighted like the ff. command.
+            e.g. \`.help\`
+        `)
         .addField('Aliases', `
             Alternate keywords for the command.
             ie. \`${PREFIX}lineup add\` = \`${PREFIX}add\`
@@ -43,13 +50,21 @@ function help(commandInputs : CommandInputs) {
         `);
 
     Object.entries(helpDescriptions).forEach(([category, commands]) => {
-        helpEmbed.addField(`${category} Commands`, '\u200b');
-        commands.forEach(({ name, value }) => {
-            helpEmbed.addField(name, value, true);
-        });
+        const categoryField = `${category} Commands`;
+
+        simpleHelpEmbed.addField(
+            categoryField,
+            commands.map(({ simpleDescription }) => simpleDescription).join('\n'),
+        );
+
+        helpEmbed.addField(categoryField, '\u200b');
+        commands.forEach(({ name, description }) => helpEmbed.addField(name, description, true));
     });
 
-    sendMessage(message.channel, helpEmbed, () => {});
+    sendMessage(message.channel, simpleHelpEmbed, (sentMessage : Message) => {
+        deleteMessage(sentMessage, INFO_MSG_TIME_DEL);
+    });
+    message.author.send(helpEmbed);   
 }
 
 const helpCommands = [{
