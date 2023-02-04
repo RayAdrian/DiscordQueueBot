3a57ea1ed117beab4b41ca64259060e977b703a9
allendomingo <allen.domingo@eee.upd.edu.ph>
2023-02-04 22:59:23 +0800

    Add getGame method

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -7,4 +7,27 @@ export default class GameService {
     setRedisClient(redisClient : RedisClientType) : void {
         this.redisClient = redisClient;
     }
+
+    /**
+     * Get game object
+     * @param name - name of the game
+     */
+    getGame(name : string) : Promise<Game> {
+        const key = `game-${name}`;
+
+        return this.redisClient.get(key).then((cachedGame) => {
+            console.log('cachedGame', cachedGame);
+            if (cachedGame) {
+                return Promise.resolve(new Game(JSON.parse(cachedGame)));
+            }
+            return Games.findOne({ name }).then((rawGame) => {
+                if (rawGame) {
+                    const game = new Game(rawGame);
+                    this.redisClient.set(key, JSON.stringify(game));
+                    return game;
+                }
+                return null;
+            });
+        }).then((game) => game);
+    }
 };
