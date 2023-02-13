d1a8d33b5dd0c0ff74710c0d21d787eeb9fcf43a
allendomingo <allen.domingo@eee.upd.edu.ph>
2023-02-13 23:37:30 +0800

    Create getUser method in userService, and refactor getUserGames method

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -19,17 +19,16 @@ export default class UserService {
     }
 
     /**
-     * Get list of games saved by a user
+     * Get user
      * @param id - id of the specified user
-     * @returns list of strings of game names
+     * @returns user object
      */
-    getUserGames(id : string) : Promise<Array<string>> {
+    getUser(id : string) : Promise<User> {
         let fetchingCached = Promise.resolve(null);
         const userKey = `user-${id}`;
         if (this.isRedisEnabled) {
             fetchingCached = this.redisClient.get(userKey).then((cachedUser) => {
                 if (cachedUser) {
-                    console.log(JSON.parse(cachedUser));
                     return new User(JSON.parse(cachedUser));
                 }
                 return null;
@@ -40,26 +39,34 @@ export default class UserService {
             if (user) {
                 return Promise.resolve(user);
             }
-            return Users.findOne({ id }).then((rawUser) => {
+            return Users.findOne({ id }).then(async (rawUser) => {
                 const user = rawUser
                 ? new User(rawUser)
                 : new User({ id, gameNames: [] });
-                const wrappedUser = user.getUserWrapper();
 
                 if (!rawUser) {
-                    Users.create(wrappedUser).then(() => {
-                        if (this.isRedisEnabled) {
-                            this.redisClient.set(userKey, JSON.stringify(wrappedUser));
-                        }
-                    });
-                } else {
-                    if (this.isRedisEnabled) {
-                        this.redisClient.set(userKey, JSON.stringify(wrappedUser));
-                    }
+                    const wrappedUser = user.getUserWrapper();
+                    await Users.create(wrappedUser);
                 }
 
                 return user;
             });
-        }).then((user) => user.getGameNames());
+        }).then((user) => {
+            const wrappedUser = user.getUserWrapper();
+            if (this.isRedisEnabled) {
+                this.redisClient.set(userKey, JSON.stringify(wrappedUser));
+            }
+
+            return user;
+        });
+    }
+
+    /**
+     * Get list of games saved by a user
+     * @param id - id of the specified user
+     * @returns list of strings of game names
+     */
+    getUserGames(id : string) : Promise<Array<string>> {
+        return this.getUser(id).then((user) => user.getGameNames());
     }
 };
