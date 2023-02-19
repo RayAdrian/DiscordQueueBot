79ac1984b027fca4d8bb947359a8237780c38e67
allendomingo <allen.domingo@eee.upd.edu.ph>
2023-02-19 22:51:05 +0800

    Add getLineup method to LineupService

--- src/services/lineupService.ts
+++ src/services/lineupService.ts
@@ -1,4 +1,5 @@
 import { RedisClientType } from 'redis';
+import { Lineups, Lineup } from '../models/index.js';
 
 export default class LineupService {
     private redisClient: RedisClientType;
@@ -12,5 +13,40 @@ export default class LineupService {
         if (this.redisClient) {
             this.isRedisEnabled = true;
         }
+    }
+
+    /**
+     * Get the specified game's Lineup
+     * @param gameName - game name of the Lineup to be retrieved
+     * @returns Promise of the requested Lineup
+     */
+    getLineup(gameName : string) : Promise<Lineup> {
+        let fetchingCached : Promise<Lineup> = Promise.resolve(null);
+        const lineupKey = `lineup-${gameName}`;
+        if (this.isRedisEnabled) {
+            fetchingCached = this.redisClient.get(lineupKey).then((cachedLineup) => {
+                if (cachedLineup) {
+                    console.log(`${lineupKey} hit`);
+                    return new Lineup(JSON.parse(cachedLineup));
+                }
+                return null;
+            });
+        }
+    
+        return fetchingCached.then((lineup) => {
+            if (lineup) {
+                return Promise.resolve(lineup);
+            }
+            return Lineups.findOne({ gameName }).then(
+                (rawLineup) => new Lineup(rawLineup),
+            ).then((lineup) => {
+                if (this.isRedisEnabled) {
+                    console.log(`caching ${lineupKey}`);
+                    const wrappedLineup = lineup.getLineupWrapper();
+                    this.redisClient.set(lineupKey, JSON.stringify(wrappedLineup));
+                }
+                return lineup;
+            });
+        }).then((lineup) => lineup);
     }
 };
